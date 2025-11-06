2ed9fa3b817b21bfad2c0f9e615232e49c8df12a
Elias Teotonio <eliascalado@hotmail.com>
2025-11-06 19:11:19 -0300

    backend project created

---
+++ backend/src/main/java/com/etc/sedevendas/SedevendasApplication.java
@@ -0,0 +1,13 @@
+package com.etc.sedevendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SedevendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SedevendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/etc/sedevendas/SedevendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.etc.sedevendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SedevendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
